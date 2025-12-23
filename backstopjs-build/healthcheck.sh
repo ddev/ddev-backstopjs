cf13191c8b86d1a21d61a2729b9347f448e6de25
Michael Waltonen <waltonen@umn.edu>
2025-12-23 15:01:40 -0600

    Switch BACKSTOPJS_ALLOW_PERM_REPAIR to form that works with ddev dotenv

--- backstopjs-build/healthcheck.sh
+++ backstopjs-build/healthcheck.sh
@@ -9,7 +9,7 @@ DDEV_GROUP="$(id -gn)"
 # Confirm /src owner and group matches container user. Attempt to fix, if BACKSTOPJS_ALLOW_PERM_REPAIR is true.
 # This is to handle cases where BACKSTOPJS_TESTDIR is changed to a directory that does not exist and docker auto-creates it.
 if ! [ "$VOLUME_OWNER" = "$DDEV_USER" ] || ! [ "$VOLUME_GROUP" = "$DDEV_GROUP" ]; then
-    if ${BACKSTOPJS_ALLOW_PERM_REPAIR:-false}; then
+    if [ "${BACKSTOPJS_ALLOW_PERM_REPAIR:-"false"}" = "true" ]; then
         if ! sudo chown -R "$DDEV_USER":"$DDEV_GROUP" /src &> /dev/null; then
             printf "Automated repair failed. %s owner/group is %s/%s, should be %s/%s. On host, run 'sudo chown -R %s:%s %s'." "$BACKSTOPJS_TESTDIR" "$VOLUME_OWNER" "$VOLUME_GROUP" "$DDEV_USER" "$DDEV_GROUP" "$DDEV_USER" "$DDEV_GROUP" "$BACKSTOPJS_TESTDIR"
             exit 1
